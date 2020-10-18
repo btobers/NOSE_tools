9469447eecb3f8305add2bbc0537797e057d681c
btobers <tobers.brandon@gmail.com>
2020-10-18 15:05:23 -0700

    merge picks script

---
+++ nose_pick_combine.py
@@ -0,0 +1,57 @@
+import numpy as np
+import pandas as pd
+import glob
+import sys
+
+# script designed to merge the picks from one directory in to a single csv file
+# BST, 27NOV19
+
+in_dir = "/media/btober/beefmaster/MARS/targ/supl/UAF/2016/picks/"
+out_fname = "2016_combined.csv"
+
+track = np.array([]).astype(str)
+trace = np.array([]).astype(int)
+lon = np.array([]).astype(np.float)
+lat = np.array([]).astype(np.float)
+alt = np.array([]).astype(np.float)
+gndElev = np.array([]).astype(np.float)
+srfIdx = np.array([]).astype(np.float)
+srfTwtt = np.array([]).astype(np.float)
+srfAmp = np.array([]).astype(np.float)
+subsrfIdx = np.array([]).astype(np.float)
+subsrfTwtt = np.array([]).astype(np.float)
+subsrfAmp = np.array([]).astype(np.float)
+subsrfElev = np.array([]).astype(np.float)
+thick = np.array([]).astype(np.float)
+
+
+for file in glob.glob(in_dir + "*pk.csv"):
+    fname = file.split('/')[-1].rstrip("_pk.csv")
+    print(fname)
+
+    dat = np.genfromtxt(file, delimiter=",", dtype = None, names = True)
+
+    track = np.append(track, np.repeat(fname,dat.shape[0]))
+    trace = np.append(trace,dat["trace"].astype(int))
+    lon = np.append(lon,dat["lon"])
+    lat = np.append(lat,dat["lat"])
+    alt = np.append(alt,dat["alt"])
+    gndElev = np.append(gndElev,dat["gndElev"])
+    srfIdx = np.append(srfIdx, dat["srfIdx"])
+    srfTwtt = np.append(srfTwtt,dat["srfTwtt"])
+    srfAmp = np.append(srfAmp,dat["srfAmp"])
+    subsrfIdx = np.append(subsrfIdx,dat["subsrfIdx"])
+    subsrfTwtt = np.append(subsrfTwtt,dat["subsrfTwtt"])
+    subsrfAmp = np.append(subsrfAmp,dat["subsrfAmp"])
+    subsrfElev = np.append(subsrfElev,dat["subsrfElev"])
+    thick = np.append(thick,dat["thick"])
+
+
+out = pd.DataFrame({"trace": trace, "lon": lon, "lat": lat, "alt": alt, "gndElev": gndElev,
+                    "srfIdx": srfIdx, "srfTwtt": srfTwtt, "srfAmp": srfAmp, 
+                    "subsrfIdx": subsrfIdx, "subsrfTwtt": subsrfTwtt, 
+                    "subsrfAmp": subsrfAmp, "subsrfElev": subsrfElev, "thick": thick})
+
+out.to_csv(in_dir + out_fname, index=False)
+
+print("Combined picks exported to:\t" + in_dir + out_fname)
--- nose_pickfile_update.py
+++ nose_pickfile_update.py
@@ -8,15 +8,37 @@ import glob
 import pandas as pd
 import numpy as np
 
-in_path = "/path/to/pick/files/"
-in_path="/home/btober/Desktop/test/"
+in_path = "/media/btober/beefmaster/MARS/targ/supl/UAF/2016/picks/"
 
 for f in glob.glob(in_path + "*.csv"):
+  df_out = None
   print(f)
   df = pd.read_csv(f,delimiter=",",index_col=False,header=0)
-  df_out = pd.DataFrame({"trace": df["trace"], "lon": df["lon"], "lat": df["lat"], "alt": df["alt"], "gndElev": df["elev_gnd"],
-                         "srfIdx": np.nan, "srfTwtt": df["twtt_surf"], "srfAmp": df["surf_amp"], 
-                         "subsrfIdx": np.nan, "subsrfTwtt": df["twtt_bed"], "subsrfAmp": df["subsurf_amp"], 
-                         "subsrfElev": df["elev_bed"], "thick": df["thick"]})
+  
+  # determine if df has alt or elev_air
+  if "alt" in df.keys():
+    alt = df["alt"]
+  else:
+    alt = df["elev_air"]
 
-  df_out.to_csv(f.rstrip(".csv") + "_new.csv", index=False)
+  # determine if input df has amp data
+  if "surf_amp" in df.keys():
+    srfAmp = df["surf_amp"]
+    subsrfAmp = df["subsurf_amp"]
+  else:
+    srfAmp = np.nan
+    subsrfAmp = np.nan
+
+  if "subsrfElev" in df.keys():
+    continue
+
+
+  try:
+    df_out = pd.DataFrame({"trace": df["trace"], "lon": df["lon"], "lat": df["lat"], "alt": alt, "gndElev": df["elev_gnd"],
+                          "srfIdx": np.nan, "srfTwtt": df["twtt_surf"], "srfAmp": srfAmp, 
+                          "subsrfIdx": np.nan, "subsrfTwtt": df["twtt_bed"], "subsrfAmp": subsrfAmp, 
+                          "subsrfElev": df["elev_bed"], "thick": df["thick"]})
+  except Exception as err:
+    print(err)
+
+  df_out.to_csv(f, index=False)
